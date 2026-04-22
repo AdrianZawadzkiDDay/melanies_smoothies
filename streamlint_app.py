1dc7f66899b866013e8520e608c8cc39635adc55
AdrianZawadzkiDDay <45298383+AdrianZawadzkiDDay@users.noreply.github.com>
2026-04-22 18:25:17 +0200

    Update streamlint_app.py

--- streamlint_app.py
+++ streamlint_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -13,7 +12,9 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 ingredients_list = st.multiselect(
@@ -24,7 +25,6 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
